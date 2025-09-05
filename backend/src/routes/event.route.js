857dc059c88a75d3c2f89607bca06fa36b97ce5a
behan02 <behancrick@gmail.com>
2025-09-05 13:16:06 +0530

    Create protect route method

--- backend/src/routes/event.route.js
+++ backend/src/routes/event.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createEvent, deleteEvent, getEvents, updateEvent } from "../controllers/event.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/fetch", getEvents);
-router.post("/create", createEvent);
-router.delete("/delete/:id", deleteEvent);
-router.put("/update/:id", updateEvent);
+router.post("/create", protectRoute, createEvent);
+router.delete("/delete/:id", protectRoute, deleteEvent);
+router.put("/update/:id", protectRoute, updateEvent);
 
 export default router;
