6c2c6833d914b115fb21688a5749955d041edbe5
Behan Inuka <behancrick@gmail.com>
2026-01-03 01:44:54 +0530

    Update CORS origin to production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://main.d1blm9ncda9np.amplifyapp.com",
     credentials: true
 }));
 
