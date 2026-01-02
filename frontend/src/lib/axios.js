e887fa0283d93006b60aa42be05cee8ed15797ce
Behan Inuka <behancrick@gmail.com>
2026-01-03 01:39:07 +0530

    Update API base URL to production server

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "13.60.15.165:3000/api";
 
 export const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
